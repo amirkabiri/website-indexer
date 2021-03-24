dcba7d69a429beb58ba40b9a93285043e85d8291
Amir Kabiri <akabiridev@gmail.com>
2021-03-24 21:30:27 +0430

    sockets/index.js : addURLToQueue options

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -4,6 +4,9 @@ import addURLToQueue from "../libs/addURLToQueue";
 export const ROUTE = 'index';
 
 export default socket => async startPoint => {
-  await addURLToQueue(startPoint);
+  await addURLToQueue(startPoint, {
+    priority: 2,
+    depth: 2,
+  });
   emit(startPoint);
 };
